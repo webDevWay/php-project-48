4574a7c5f97c358f4f8d728bfb7fcf78f8192e5d
WebDevWay <k.vasiliy.r@gmail.com>
2025-11-07 21:37:59 +0800

    3 commit

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -6,48 +6,45 @@ namespace Differ\Formatters;
 
 function formatStylish(array $diff): string
 {
-    return buildOutput($diff);
+    return iter($diff, 1);
 }
 
-function buildOutput(array $diff, int $depth = 0): string
+function iter(array $diff, int $depth): string
 {
-    $lines = array_map(function ($node) use ($depth) {
-        return formatNode($node, $depth);
+    $indent = buildIndent($depth);
+    $bracketIndent = buildIndent($depth - 1);
+
+    $lines = array_map(function ($node) use ($depth, $indent) {
+        $type = $node['type'];
+        $key = $node['key'];
+
+        switch ($type) {
+            case 'added':
+                $value = stringify($node['value'], $depth);
+                return "{$indent}+ {$key}: {$value}";
+            case 'removed':
+                $value = stringify($node['value'], $depth);
+                return "{$indent}- {$key}: {$value}";
+            case 'unchanged':
+                $value = stringify($node['value'], $depth);
+                return "{$indent}  {$key}: {$value}";
+            case 'changed':
+                $oldValue = stringify($node['oldValue'], $depth);
+                $newValue = stringify($node['newValue'], $depth);
+                return "{$indent}- {$key}: {$oldValue}\n{$indent}+ {$key}: {$newValue}";
+            case 'nested':
+                $children = iter($node['children'], $depth + 1);
+                return "{$indent}  {$key}: {$children}";
+            default:
+                throw new \Exception("Unknown type: {$type}");
+        }
     }, $diff);
 
-    return "{\n" . implode("\n", $lines) . "\n" . indent($depth) . "}";
-}
-
-function formatNode(array $node, int $depth): string
-{
-    $key = $node['key'];
-
-    switch ($node['type']) {
-        case 'nested':
-            $children = buildOutput($node['children'], $depth + 1);
-            return indent($depth) . "  {$key}: {$children}";
-
-        case 'added':
-            $value = stringify($node['value'], $depth + 1);
-            return indent($depth) . "+ {$key}: {$value}";
-
-        case 'removed':
-            $value = stringify($node['value'], $depth + 1);
-            return indent($depth) . "- {$key}: {$value}";
-
-        case 'changed':
-            $oldValue = stringify($node['oldValue'], $depth + 1);
-            $newValue = stringify($node['newValue'], $depth + 1);
-            return indent($depth) . "- {$key}: {$oldValue}\n" .
-                   indent($depth) . "+ {$key}: {$newValue}";
-
-        case 'unchanged':
-            $value = stringify($node['value'], $depth + 1);
-            return indent($depth) . "  {$key}: {$value}";
-
-        default:
-            throw new \Exception("Unknown node type: {$node['type']}");
-    }
+    return implode("\n", [
+        "{",
+        ...$lines,
+        "{$bracketIndent}}"
+    ]);
 }
 
 function stringify(mixed $value, int $depth): string
@@ -64,16 +61,22 @@ function stringify(mixed $value, int $depth): string
         return (string) $value;
     }
 
-    $lines = [];
-    foreach ($value as $key => $val) {
-        $formattedValue = stringify($val, $depth + 1);
-        $lines[] = indent($depth) . "    {$key}: {$formattedValue}";
-    }
+    $indent = buildIndent($depth);
+    $bracketIndent = buildIndent($depth - 1);
 
-    return "{\n" . implode("\n", $lines) . "\n" . indent($depth - 1) . "  }";
+    $lines = array_map(function ($key, $val) use ($depth, $indent) {
+        $formattedValue = stringify($val, $depth + 1);
+        return "{$indent}  {$key}: {$formattedValue}";
+    }, array_keys($value), $value);
+
+    return implode("\n", [
+        "{",
+        ...$lines,
+        "{$bracketIndent}  }"
+    ]);
 }
 
-function indent(int $depth): string
+function buildIndent(int $depth): string
 {
     return str_repeat('    ', $depth);
 }
