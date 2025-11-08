f694dc10ac277642463544f0743fa860834caf5a
WebDevWay <k.vasiliy.r@gmail.com>
2025-11-08 21:42:06 +0800

    3.5

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -6,5 +6,11 @@ namespace Differ\Formatters\Json;
 
 function format(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    $json = json_encode($diff, JSON_PRETTY_PRINT);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        throw new \Exception("JSON encoding error: " . json_last_error_msg());
+    }
+
+    return $json;
 }
